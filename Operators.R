e36ffa8b9c7b0c191b1e9d7a3a5616516fcb428b
Taylor Heyen <taylordheyen@gmail.com>
2025-05-27 09:48:07 -0700

    Add files via upload

---
+++ Operators.R
@@ -0,0 +1,55 @@
+a <- 15
+
+b <- 7
+
+# Math Operators
+a + b
+
+a - b
+
+a * b
+
+a / b
+
+a %% b
+
+a ^ b
+
+# Comparison Operators
+
+a > 5
+a > b
+
+b > a
+
+a >= b
+
+10 >= 10
+
+a < 5
+
+a != b
+
+5 != 5
+
+
+# Logical Operators
+
+x <- TRUE
+
+z <- FALSE
+
+x & z
+
+a > 5 & b > 2
+
+x | z
+
+a > 5 | b > 100
+
+!x
+
+! a > 100
+
+
+a > 10 & (b > 10 | b > 5)
